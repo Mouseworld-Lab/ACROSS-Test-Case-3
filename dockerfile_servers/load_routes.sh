461a22fdfc69b1553a508655b1106aaa9dc487fb
Mario Sanz Rodrigo <85732572+MarioSanzRodrigoUPM@users.noreply.github.com>
2024-09-04 13:53:12 +0200

    Add files via upload

---
+++ dockerfile_servers/load_routes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+hostname=$(cat /etc/hostname)
+
+number=$(echo $hostname | grep -o '[0-9]\+')
+
+if [[ $hostname == client* ]]; then
+    sudo python3 "/home/across/routes/routes_client_$number.py"
+elif [[ $hostname == server* ]]; then
+    sudo python3 "/home/across/routes/routes_server_$number.py"
+else
+    echo "Hostname no reconocido: $hostname"
+fi
