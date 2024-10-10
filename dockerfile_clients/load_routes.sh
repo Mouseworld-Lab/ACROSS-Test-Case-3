b2c367fe40878870489a82ba50d361d980f918ed
Mario Sanz Rodrigo <85732572+MarioSanzRodrigoUPM@users.noreply.github.com>
2024-10-10 14:24:33 +0200

    Add files via upload

--- dockerfile_clients/load_routes.sh
+++ dockerfile_clients/load_routes.sh
@@ -4,8 +4,8 @@ hostname=$(cat /etc/hostname)
 
 number=$(echo $hostname | grep -o '[0-9]\+')
 
-if [[ $hostname == client* ]]; then
-    sudo python3 "/home/across/routes/routes_client_$number.py"
+if [[ $hostname == probe* ]]; then
+    sudo python3 "/home/across/routes/routes_probe_$number.py"
 elif [[ $hostname == server* ]]; then
     sudo python3 "/home/acrossroutes/routes_server_$number.py"
 else
