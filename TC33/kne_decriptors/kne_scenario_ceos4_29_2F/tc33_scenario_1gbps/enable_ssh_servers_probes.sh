d57da82a1cc47be5f3551ba1d6819a4a86f51ee0
Mario Sanz Rodrigo <85732572+MarioSanzRodrigoUPM@users.noreply.github.com>
2025-05-08 11:38:45 +0200

    Add files via upload

---
+++ TC33/kne_decriptors/kne_scenario_ceos4_29_2F/tc33_scenario_1gbps/enable_ssh_servers_probes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+kubectl -n across-tc33 exec -it server1 -- service ssh start
+kubectl -n across-tc33 exec -it server2 -- service ssh start
+kubectl -n across-tc33 exec -it server3 -- service ssh start
+
+kubectl -n across-tc33 exec -it probe1 -- service ssh start
+kubectl -n across-tc33 exec -it probe2 -- service ssh start
+kubectl -n across-tc33 exec -it probe3 -- service ssh start
+kubectl -n across-tc33 exec -it probe4 -- service ssh start
+kubectl -n across-tc33 exec -it probe5 -- service ssh start
+kubectl -n across-tc33 exec -it probe6 -- service ssh start
+kubectl -n across-tc33 exec -it probe7 -- service ssh start
+kubectl -n across-tc33 exec -it probe8 -- service ssh start
+kubectl -n across-tc33 exec -it probe9 -- service ssh start
+
+kubectl -n across-tc33 exec -it server1 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it server2 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it server3 -- bash -c ". /home/across/load_routes.sh"
+
+kubectl -n across-tc33 exec -it probe1 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe2 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe3 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe4 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe5 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe6 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe7 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe8 -- bash -c ". /home/across/load_routes.sh"
+kubectl -n across-tc33 exec -it probe9 -- bash -c ". /home/across/load_routes.sh"
